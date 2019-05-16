906f85fb3bfb71cbeacdebc375c0f431ede3796e
David2Santos <48089104+David2Santos@users.noreply.github.com>
2019-05-16 13:11:16 +0200

    Add files via upload

---
+++ recibo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct recibo {
+	int precio;
+	char* p;
+};
+int main() {
+	int i, n;
+	FILE *recibo;
+	struct recibo cliente;
+	cliente.precio = 5;
+	errno_t err1;
+	n = 12;
+	cliente.p = (char *)malloc(n+1 * sizeof(char));
+	fgets(cliente.p, n + 1, stdin);
+	err1 = fopen_s(&recibo, "recibo.txt", "a+");
+	if (err1 == 0)
+		printf("funciona");
+	else
+		printf("no funciona");
+
+	fprintf(recibo, "Recibo de su pedido:\n");
+	fprintf(recibo, "%s: %d€\n", cliente.p, cliente.precio);
+	fclose(recibo);
+}
+
